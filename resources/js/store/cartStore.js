46e58b0e71242973a9a14827770cf4cabe5c785b
maxim.anichienco <maxim.anichienco@ctif.gov.md>
2026-05-07 15:45:07 +0300

    App-market WIP

--- resources/js/store/cartStore.js
+++ resources/js/store/cartStore.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
-import CartService from '@/services/CartService';
+import CartService from './../services/cartService.js';
 
 export const useCartStore = defineStore('cart', () => {
     const items = ref([]);
